f64ce6a1f3aaf8e038cb3445a81f0017ef280344
AlessS4ndro <axelito_b.b@hotmail.com>
2017-10-19 14:56:38 -0500

    trabajo terminado

--- afin.h
+++ afin.h
@@ -6,8 +6,8 @@ class Afin
   int key_a;
   int key_b;
 public:
-  Afin():alfabeto("abcdefghijklmnopqrstuvwxyz"){}
-  Afin(int a,int b):key_a(a),key_b(b),alfabeto("abcdefghijklmnopqrstuvwxyz"){}
+  Afin():alfabeto("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ,._-0123456789"){}
+  Afin(int a,int b):key_a(a),key_b(b),alfabeto("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ,._-0123456789"){}
   string cifrar(string);
   string descifrar(string);
 };
@@ -17,23 +17,22 @@ string Afin::cifrar(string mensaje)
 {
   string mensajeNuevo="";
   int x;
-  int size=alfabeto.length();cout<<"el size es: "<<size<<endl;
-
+  int size=alfabeto.length();
   do{
     key_a=rand();                //////////////completar con la funcion del p okemon
     key_a=modulo(key_a,size);
-  }while(euclides(size,key_a) != 1);cout<<"el key_a es: "<<key_a<<endl;
+  }while(euclides(size,key_a) != 1);
   do{
     key_b=rand();              ////////    completar con la funcion del pokemon
     key_b=modulo(key_b,size);
-  }while(key_b<0);cout<<"el key_b es<: "<<key_b<<endl;
-  
+  }while(key_b<0);
+
   for (int i=0;i<mensaje.length();i++){
-    x=alfabeto.find(mensaje[i]);cout<<" primer valor de x : "<<x<<endl;
-    x=x*key_a;cout<<"x antes del primer modulo 26"<<x<<endl;
+    x=alfabeto.find(mensaje[i]);
+    x=x*key_a;
     x=modulo(x,size);
 
-    x=x+key_b;cout<<"x antes del segundo modulo 26 "<<x<<endl;
+    x=x+key_b;
     x=modulo(x,size);
     mensajeNuevo+=alfabeto[x];
   }
@@ -44,15 +43,15 @@ string Afin::descifrar(string mensaje)
 {
   string mensajeNuevo="";
   int y;
-  int size=alfabeto.length();cout<<"el size es: "<<size<<endl;
+  int size=alfabeto.length();
   long c,inversa_a,inversa_b;
 
   for (int i=0;i<mensaje.length();i++){
-    y=alfabeto.find(mensaje[i]);cout<<" primer valor de y: "<<y<<endl;
-    y=y-key_b;cout<<"y antes del primer modulo 26 "<<y<<endl;
+    y=alfabeto.find(mensaje[i]);
+    y=y-key_b;
     y=modulo(y,size);
-    euclides_extendido(key_a,size,c,inversa_a,inversa_b);cout<<"key_a es: "<<key_a<<" inversa a es :"<<inversa_a<<" inversa_b es : "<<inversa_b<<endl;
-    y=inversa_a*y;cout<<"y antes del segundo moudlo 26 "<<y<<endl;
+    euclides_extendido(key_a,size,c,inversa_a,inversa_b);
+    y=inversa_a*y;
     y=modulo(y,size);
     mensajeNuevo+=alfabeto[y];
   }
