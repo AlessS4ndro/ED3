39276ddebb4cc89b7e0863e12348f334c7443a2d
AlessS4ndro <axelito_b.b@hotmail.com>
2017-10-22 21:57:59 -0500

    trabajo faltante

--- euclides.cpp
+++
@@ -1,7 +0,0 @@
-#include "funciones.h"
-int euclides(int a,int b)
-{
-  if(b==0)
-    return a;
-  return euclides(b,module(a,b));
-}
--- modulo.cpp
+++
@@ -1,8 +0,0 @@
-int module(int value,int modulo)
-{
-	int cociente=value/modulo;
-
-	if(value>0)
-		return value-cociente*modulo;
-	return modulo-(value*-1+cociente*modulo);
-}
