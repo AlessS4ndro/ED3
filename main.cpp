f09b97aa8dcc8669cd591cabde5cb7933842f3b5
AlessS4ndro <axelito_b.b@hotmail.com>
2017-11-04 22:00:56 -0500

    terminar el rsa

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
-
+#include <NTL/ZZ.h>
 
 using namespace std;
+using namespace NTL;
 #include "funciones.h"
 #include "cesar.h"
 #include "afin.h"
@@ -17,11 +18,9 @@ int main(int argc,char ** argv)
   Afin emisor1;
   string message=emisor1.cifrar(argv[1]);
   cout<<message<<endl;
-
   Afin emisor;
   string message=emisor.cifrar("Hi , how old are you. mi name is Alex Mamani cui 20153696 soy de la ciudad de puno york estudio en la unsa facultad de ingenieria de produccion y servicios en la escuela de ciencia de la computacion.ermission is granted to copy, distribute and/or modify this document under the terms of the GNU Free Documentation License1.1 or any later version published by the Free Software Foundation; with no Invariant Sections, no Front-Cover Texts and no Back-Cover Texts. A copy of the license is included in the section entitled GNU Free Documentation License See Appendix Documentation License, page 197, for details.");
   cout<<message<<endl;
-
   Afin receptor1(21,7);
   cout<<receptor1.descifrar("EHLhBXH1HByYehYB1PBmH1CDyByYBUPXeHDB1hmzyLB5hmh1.hBXhmhuzPDyBQB1PBXzPByYBO8t08lKGByYeyByYBzmB1ymYh5yBdyB.DzyCh")<<endl;
   cout<<"\n\n\n";
@@ -31,10 +30,14 @@ int main(int argc,char ** argv)
   Afin receptor3(44,33);
   cout<<receptor3.descifrar("nOxiSYzic8H8YUcRvQDDiv8vR8gH8R8giRYzxSUtiFRHWDUSY8gzRQvQHWWrR8HeivR i6iSRv8ixvR8gHc8giRnQ.WYFiHcRHWDUSY8gzuRHcFRYcR8giRSizHYcYcDR.HvivR8giRcQzkiSRU Rv8ixvRSizHYcR8givHzi7RdgiRSiHFiSR6gUR6YvgivR8URiOxiSYzic8R6Y8gR8giviRHWDUSY8gzvRzHrR.UcvYFiSRQvYcD8giR UWWU6YcDREH8WHkRSUQ8YcivuR6gY.gRUQ8xQ8R8giRcQzkiSRU Rv8ixvR8HeicRHvR6iWWRHvR8giDSiH8iv8R.UzzUcRFYtYvUS7R_cRkU8gRSUQ8YcivR8giR YSv8RWYcivR8HeiR8giRHkvUWQ8iRtHWQivRU RHRXMV,RnKIMXRfV_ACnRdZdZ2MwZ")<<endl;
   cout<<"\n\n\n";
-  
+
   Afin receptor4(atoi(argv[1]),atoi(argv[2]));
   cout<<receptor4.descifrar(argv[3])<<endl;
   */
-  //RSA receptor;
-  
+  string palabra="a";
+  ZZ e,n;
+  e=201123333;n=41234132;
+  RSA emisor(n,e);
+  emisor.cifrar(palabra);
+
 }
--- rsa.h
+++ rsa.h
@@ -16,7 +16,6 @@ public:
   RSA(ZZ n,ZZ e){this->n=n;this->e=e;alfabeto="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";}
   string cifrar(string);
   string decifrar(string);
-
 };
 
 RSA::RSA()
@@ -24,7 +23,7 @@ RSA::RSA()
   alfabeto="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   ZZ p,q,fiN,d,x,y;
   ZZ c[6];
-  
+
   do{
     p=RandomLen_ZZ(8);cout<<p<<endl;
     q=RandomLen_ZZ(8);cout<<q<<endl;
@@ -32,12 +31,12 @@ RSA::RSA()
 
   n=p*q;
   fiN=(p-1)*(q-1);
-  
+
   do{
-    e=RandomLen_ZZ(16);  /// escogemos un numero e  
+    e=RandomLen_ZZ(16);  /// escogemos un numero e
     e=modulo_ZZ(e,fiN);   //// q sea coprimo con fiN
   }while(euclides_ZZ(fiN,e)!=1);
- 
+
   euclides_extendido_ZZ(fiN,e,d,x,y); /////// hallamos la inversa de fiN
   if(x<0){
     if(fiN>e)x+=fiN;
@@ -49,7 +48,7 @@ RSA::RSA()
 }
 
 string RSA::cifrar(string tp)
-{
+{/*
   ofstream texto("cifradoRSA.txt");
   string mensajecifrado="";
   ZZ m,size,digitosAlfabeto,pos;
@@ -57,7 +56,7 @@ string RSA::cifrar(string tp)
   size=alfabeto.length();
 
   digitosAlfabeto=number_digits(size-1); //numero maximo de digitos del alfabeto '2' en caso de 26 letras
-  
+
   for(int i=0;i<tp.length();i++){ // recorremos el mensaje
     pos=tp.find(tp[i]);  // almacenamos la posicion del caracter
     if(number_digits(pos)!=digitosAlfabeto){
@@ -68,7 +67,7 @@ string RSA::cifrar(string tp)
       mensajecifrado+=int_to_string(pos);
     }
   }
-  
+
   for (int i=0;i<tp.length();i++){
     m=tp.find(tp[i]);
     t=ZZ_to_int(modulo_ZZ(exponenciacion_modular(e,n,m),size));
@@ -77,6 +76,11 @@ string RSA::cifrar(string tp)
   texto<<mensajecifrado;
   texto.close();
   return mensajecifrado;
+
+  //ZZ m;
+  //m=alfabeto.find(tp);
+  //cout<<"m es: "<<m;
+//  cout<<exponenciacion_modular(m,e,n);
+*/
+
 }
-
-
